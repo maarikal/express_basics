4a6481c673d286779af2cbec5c6b0844bbcf2913
Maarika Lukk <maarika.lukk@khk.ee>
2022-02-10 11:43:08 +0200

    post request use

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extend:true}));
+
 // add directory to public folder
 app.use(express.static('public'));
 //add views directory path
 app.set('views', path.join(__dirname, 'views'));
 // add views template engine
 app.set('view engine', 'ejs');
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    if(username === 'user' && password === 'qwerty') {
+        res.redirect('/dashboard');
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
 
 
 app.get('/questions', (req, res) => {
